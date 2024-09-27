b3803c979dcfa1fdd5e379f59b0a3df2fc05c5f6
Chico Han <minhanacc@gmail.com>
2024-09-27 09:59:22 +0630

    Initial Commit

---
+++ RoboList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoboList
+//
+//  Created by Chico Han on 27/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoboList/RoboListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoboListApp.swift
+//  RoboList
+//
+//  Created by Chico Han on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoboListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
